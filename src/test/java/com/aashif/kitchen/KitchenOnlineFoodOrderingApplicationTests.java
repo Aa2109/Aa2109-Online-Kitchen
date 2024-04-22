d5170a76007fcbb8a7af0d82059aced6c424cb1c
Aashif <aashifiqubal68@gmail.com>
2024-04-23 01:50:10 +0530

    initial commmit

---
+++ src/test/java/com/aashif/kitchen/KitchenOnlineFoodOrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aashif.kitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchenOnlineFoodOrderingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
